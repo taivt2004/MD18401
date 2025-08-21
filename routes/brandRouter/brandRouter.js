5fd6fe5b45148c9ea46bbba77c9f9d51e8f617ef
taivt2004 <vtt2004abc@gmail.com>
2025-08-21 11:37:09 +0700

    h

--- routes/brandRouter/brandRouter.js
+++ routes/brandRouter/brandRouter.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const brandController = require('../../controller/brandController/brand_controller');
 
-
+router.use((req, res, next) => {
+    console.log('Route /brands hit:', req.method, req.url);
+    next();
+});
 
 router.get('/all', brandController.getAllBrands);
 router.post('/add', brandController.addBrand);
